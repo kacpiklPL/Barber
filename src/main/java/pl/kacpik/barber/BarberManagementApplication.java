098efa8d0b11762bee725c81a669b3e52f9569f7
kacpiklPL <kacpikl@o2.pl>
2025-04-10 09:55:47 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/pl/kacpik/barber/BarberManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.kacpik.barber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kacpik/barber/BarberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kacpik.barber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
