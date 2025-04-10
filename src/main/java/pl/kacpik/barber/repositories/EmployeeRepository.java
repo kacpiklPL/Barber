af57cb8e2937119c481515f287a6b09c68fe2a22
kacpiklPL <kacpikl@o2.pl>
2025-04-10 10:55:17 +0200

    Add EmployeeRepository interface

---
+++ src/main/java/pl/kacpik/barber/repositories/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package pl.kacpik.barber.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.kacpik.barber.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, String> {
+
+
+
+}
