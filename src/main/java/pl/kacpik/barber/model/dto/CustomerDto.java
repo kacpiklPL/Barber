ee7933a4fac1a4f34dc760cab36834d7a1c5b60d
kacpiklPL <kacpikl@o2.pl>
2025-04-16 11:32:28 +0200

    Add CustomerDto class

---
+++ src/main/java/pl/kacpik/barber/model/dto/CustomerDto.java
@@ -0,0 +1,22 @@
+package pl.kacpik.barber.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerDto {
+
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+}
