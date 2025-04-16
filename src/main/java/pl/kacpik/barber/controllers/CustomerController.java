9147795d613a7de6f61153774aa24ff81bb4ffce
kacpiklPL <kacpikl@o2.pl>
2025-04-16 11:49:03 +0200

    Add createCustomer POST method with test for CustomerController

--- src/main/java/pl/kacpik/barber/controllers/CustomerController.java
+++ src/main/java/pl/kacpik/barber/controllers/CustomerController.java
@@ -1,8 +1,12 @@
 package pl.kacpik.barber.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.kacpik.barber.mappers.CustomerMapperImpl;
+import pl.kacpik.barber.model.Customer;
+import pl.kacpik.barber.model.dto.CustomerDto;
 import pl.kacpik.barber.services.CustomerService;
 
 @RestController
@@ -13,4 +17,12 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+
+    @PostMapping(path = "/customers")
+    public ResponseEntity<CustomerDto> createCustomer(@RequestBody CustomerDto customerDto){
+        Customer customer = customerMapper.mapFrom(customerDto);
+        Customer saveCustomer = customerService.addCustomer(customer);
+        return new ResponseEntity<>(customerMapper.mapTo(saveCustomer), HttpStatus.CREATED);
+    }
+
 }
--- src/test/java/pl/kacpik/barber/controlers/CustomerControllerTest.java
+++ src/test/java/pl/kacpik/barber/controlers/CustomerControllerTest.java
@@ -1,9 +1,14 @@
 package pl.kacpik.barber.controlers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -11,5 +16,21 @@ public class CustomerControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    public void shouldCreateNewCustomer() throws Exception{
+        String jsonValue = """
+                {
+                    "name": "Kacper",
+                    "lastName": "Nowak",
+                    "phoneNumber": "000000000"
+                }
+                """;
+
+        mockMvc.perform(post("/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonValue))
+                .andExpect(status().isCreated());
+    }
 
 }
