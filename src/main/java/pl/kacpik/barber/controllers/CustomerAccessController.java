d8caeab1ab365ff4bbda9c95bee9df294f0da18b
kacpiklPL <kacpikl@o2.pl>
2025-04-28 18:07:01 +0200

    Add getCustomerAccess GET method with test for CustomerAccessController

--- src/main/java/pl/kacpik/barber/controllers/CustomerAccessController.java
+++ src/main/java/pl/kacpik/barber/controllers/CustomerAccessController.java
@@ -1,8 +1,20 @@
 package pl.kacpik.barber.controllers;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import pl.kacpik.barber.model.Customer;
+import pl.kacpik.barber.model.CustomerAccess;
 import pl.kacpik.barber.services.CustomerAccessService;
+import pl.kacpik.barber.services.CustomerService;
+import pl.kacpik.barber.utils.QRCodeGenerator;
+
 
 @RestController
 public class CustomerAccessController {
@@ -10,4 +22,23 @@ public class CustomerAccessController {
     @Autowired
     private CustomerAccessService customerAccessService;
 
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private QRCodeGenerator qrCodeGenerator;
+
+    @GetMapping(path = "/customers/{customerId}/qrcode")
+    public ResponseEntity<byte[]> getCustomerAccess(@PathVariable Long customerId){
+        Customer customer = customerService.getCustomerById(customerId)
+                .orElseThrow(() -> new EntityNotFoundException("Customer not found with id: " + customerId));
+        CustomerAccess customerAccess = customerAccessService.getOrCreateCustomerAccess(customer);
+
+        try{
+            byte[] data = qrCodeGenerator.createQRCode("https://tapujemy.pl/qrcode/" + customerAccess.getToken());
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(data);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to generate QR code", e);
+        }
+    }
 }
