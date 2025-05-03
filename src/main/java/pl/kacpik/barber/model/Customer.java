a90b4c2a16a8ac78604952ec829c05536ce69c57
kacpiklPL <kacpikl@o2.pl>
2025-05-03 19:41:13 +0200

    remove QR code generation

--- src/main/java/pl/kacpik/barber/model/Customer.java
+++ src/main/java/pl/kacpik/barber/model/Customer.java
@@ -31,9 +31,6 @@ public class Customer {
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Reservation> reservations;
 
-    @OneToOne(mappedBy = "customer", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private CustomerAccess customerAccess;
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
